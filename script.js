2f6f6d8718003d4b3a9c6a049b12bf7ca66ba6d1
nocarium-cpu <nocarium@gmail.com>
2026-06-20 19:27:08 +0200

    Fix fetch request syntax in script.js

--- script.js
+++ script.js
@@ -324,7 +324,7 @@ async function generateRevisionSheet() {
     try {
         console.log("COURS ENVOYÉ :", course);
 
-        const response = await fetch("/generate"),
+        const response = await fetch("/generate"
             {
                 method: "POST",
                 headers: {
