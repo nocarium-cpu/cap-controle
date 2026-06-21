0eb23af0ba1887450699ac8bf9106b23c64bbffa
Skyzen <nocarium.chess@gmail.com>
2026-06-22 01:04:44 +0200

    Add sheet import

--- script.js
+++ script.js
@@ -767,18 +767,3 @@ async function importSheet() {
     );
 
 }
-
-.share-import{
-    display:flex;
-    gap:10px;
-    align-items:center;
-}
-
-#shareCode{
-    flex:1;
-}
-
-#importBtn{
-    width:auto;
-    min-width:110px;
-}
