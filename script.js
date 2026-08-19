d26e80916ee4c19fc319270278305039c52e89ec
nocarium-cpu <nocarium@gmail.com>
2026-08-19 23:07:30 +0200

    Fix date calculation by removing subtraction of 1

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function getDaysLeft(date) {
     const examDate = new Date(date);
 
     const diff =
-        examDate - today - 1;
+        examDate - today;
 
     return Math.ceil(
         diff / (1000 * 60 * 60 * 24)
